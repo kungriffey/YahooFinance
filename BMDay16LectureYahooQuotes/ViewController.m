45919c9ef1f33cb807fe326739a8a4a97a97a11e
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-27 10:42:56 -0400

    added label and button

--- BMDay16LectureYahooQuotes/ViewController.m
+++ BMDay16LectureYahooQuotes/ViewController.m
@@ -21,6 +21,14 @@
   self.textField.text = @"Insert Text Here";
   [self.view addSubview:self.textField];
   
+  //Create the Label
+  self.myLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 280, 40)];
+  [self.view addSubview:self.myLabel];
+  
+  //Create the Button
+  UIButton *myButtonPressed = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  myButtonPressed.frame = CGRectMake(20, 180, 280, 40);
+  [self.view addSubview:myButtonPressed];
 }
 
 - (void)didReceiveMemoryWarning {
