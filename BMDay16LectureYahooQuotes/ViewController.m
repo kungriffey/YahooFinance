cba5b4d22934fea5d089acce94369a50d3590328
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-27 11:02:01 -0400

    added button functionality and nsurl info

--- BMDay16LectureYahooQuotes/ViewController.m
+++ BMDay16LectureYahooQuotes/ViewController.m
@@ -18,17 +18,34 @@
   [super viewDidLoad];
   
   self.textField = [[UITextField alloc]initWithFrame:CGRectMake(20, 60, 280, 20)];
-  self.textField.text = @"Insert Text Here";
+  self.textField.text = @"AAPL";
   [self.view addSubview:self.textField];
   
   //Create the Label
   self.myLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 280, 40)];
+  self.myLabel.text = @"$0.00";
   [self.view addSubview:self.myLabel];
   
   //Create the Button
   UIButton *myButtonPressed = [UIButton buttonWithType:UIButtonTypeRoundedRect];
   myButtonPressed.frame = CGRectMake(20, 180, 280, 40);
+  [myButtonPressed setTitle:@"Press me" forState:UIControlStateNormal];
+  [myButtonPressed addTarget:self action:@selector(getQuote) forControlEvents:UIControlEventTouchUpInside];
   [self.view addSubview:myButtonPressed];
+}
+
+
+- (void)getQuote {
+  
+  NSString *quoteAddress = [NSString stringWithFormat:@"http://download.finance.yahoo.com/d/quotes.csv?s=%@&f=sl1d1t1c1ohgv&e=.csv", self.textField.text];
+  
+  //Create a URL
+  NSURL *theUrl = [[NSURL alloc]initWithString:[quoteAddress stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];
+  NSURLRequest *theRequest = [[NSURLRequest alloc]initWithURL:theUrl];
+  
+  
+  NSLog(@"%@", quoteAddress);
+  
 }
 
 - (void)didReceiveMemoryWarning {
